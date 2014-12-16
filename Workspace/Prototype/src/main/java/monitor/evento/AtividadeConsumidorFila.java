7ced8e15ab771329eb3f9ccfb8272ab46857ace4
cathenesi <cathenesi@gmail.com>
2014-12-15 23:41:07 -0200

    Atualização modelos

--- Workspace/Prototype/src/main/java/monitor/evento/AtividadeConsumidorFila.java
+++ Workspace/Prototype/src/main/java/monitor/evento/AtividadeConsumidorFila.java
@@ -1,5 +1,36 @@
 package monitor.evento;
 
-public class AtividadeConsumidorFila {
+import java.util.Date;
+
+import monitor.dominio.Propriedade;
+
+public class AtividadeConsumidorFila extends Evento {
+
+	private static final long serialVersionUID = -321848178677383710L;
+
+	private boolean ativo;
+	private int quantidadeMaximaThreads;
+	private int quantidadeAtualThreadsEmUso;
+
+	public AtividadeConsumidorFila(Propriedade propriedade, boolean ativo,
+			int quantidadeMaximaThreads, int quantidadeAtualThreadsEmUso) {
+		super(AtividadeConsumidorFila.class.getSimpleName(),
+				"Atividade Consumidor Fila", new Date(), propriedade);
+		this.ativo = ativo;
+		this.quantidadeMaximaThreads = quantidadeMaximaThreads;
+		this.quantidadeAtualThreadsEmUso = quantidadeAtualThreadsEmUso;
+	}
+
+	public boolean isAtivo() {
+		return this.ativo;
+	}
+
+	public int getQuantidadeMaximaThreads() {
+		return quantidadeMaximaThreads;
+	}
+
+	public int getQuantidadeAtualThreadsEmUso() {
+		return quantidadeAtualThreadsEmUso;
+	}
 
 }
